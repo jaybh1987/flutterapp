01dc71b93e5e84632fdf245dba731f1e234fad65
jay bhavsar <jay.bhavsar@laitmatus.com>
2021-08-05 18:16:40 +0530

    learning from this link: https://www.youtube.com/watch?v=wE7khGHVkYY&t=103s

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,67 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    const Center(
-      child: Text(
-        'Hello, world!',
-        textDirection: TextDirection.ltr,
+  runApp( new DogApp());
+}
+
+class DogApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'My Dog App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('yellow lab'),
+        ),
+        body: Center(
+
+          child: Column(
+           mainAxisAlignment: MainAxisAlignment.center,
+           children: [
+             DecoratedBox(
+                 decoration: BoxDecoration(color: Colors.green),
+                 child: Padding(
+                     padding: const EdgeInsets.all(90.0),
+                     child: Text("Rokcy")
+                 )
+             ),
+             SizedBox(height: 8.0),
+             DecoratedBox(
+                 decoration: BoxDecoration(color: Colors.green),
+                 child: Padding(
+                     padding: const EdgeInsets.all(90.0),
+                     child: Text("Jay")
+                 )
+             ),
+             SizedBox(height: 8.0),
+             DecoratedBox(
+                 decoration: BoxDecoration(color: Colors.green),
+                 child: Padding(
+                     padding: const EdgeInsets.all(90.0),
+                     child: Text("Amit")
+                 )
+             )
+           ],
+          )
+        )
+      )
+    );
+  }
+}
+
+
+class DogName extends StatelessWidget {
+  final String name;
+  const DogName(this.name);
+
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(color: Colors.lightBlueAccent),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(name),
       ),
-    ),
-  );
+    );
+  }
 }
