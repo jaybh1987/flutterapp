c6001412aed9957842ec9b6a6acdb848ea6b4ff9
jay bhavsar <jay.bhavsar@laitmatus.com>
2021-09-01 11:59:41 +0530

    change color on press is working.

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,84 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp( new DogApp());
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatefulWidget {
+    _MyState createState() => _MyState();
 }
 
-class DogApp extends StatelessWidget {
+class _MyState extends State<MyApp> {
+
+  Color _containerColor = Colors.yellow;
+
+  void changeColor() {
+    setState(() {
+        if(_containerColor == Colors.yellow) {
+          _containerColor = Colors.red;
+          return;
+        }
+        _containerColor = Colors.yellow;
+    });
+  }
+
   Widget build(BuildContext context) {
-
     return MaterialApp(
-      title: 'My Dog App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('yellow lab'),
-        ),
-        body: Center(
+      title: 'Flutter deom',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
 
-          child: Column(
-           mainAxisAlignment: MainAxisAlignment.center,
-           children: [
-             DogName("Jay"),
-             SizedBox(height: 8.0),
-             DogName("Sandy"),
-             SizedBox(height: 8.0),
-             DogName("Meet"),
-           ],
-          )
-        )
+      home: Scaffold(
+        appBar:  AppBar(
+          title: Text("A Simple App Stateful Widget"),
+        ),
+
+        body: Container(
+          decoration: BoxDecoration(color: _containerColor),
+        ),
+
+        floatingActionButton: FloatingActionButton(
+          onPressed: changeColor,
+          child: Icon(Icons.add),
+          tooltip: "Book Here",
+        ),
+
+
       )
     );
   }
 }
 
 
-class DogName extends StatelessWidget {
-  final String name;
-  const DogName(this.name);
-
-  Widget build(BuildContext context) {
-    return DecoratedBox(
-      decoration: BoxDecoration(color: Colors.lightBlueAccent),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text(name),
-      ),
-    );
-  }
-}
+// class MyApp extends StatelessWidget {
+//
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//
+//       title: 'Flutter Demo',
+//
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue
+//       ),
+//
+//       home: Scaffold(
+//
+//         appBar: AppBar(
+//           title: Text("A sample app stateless widget"),
+//         ),
+//
+//         body: Container(
+//           decoration: BoxDecoration(color: Colors.green),
+//         ),
+//
+//
+//         floatingActionButton: FloatingActionButton(
+//           child: Icon(Icons.add),
+//           tooltip: "Book Here",
+//           onPressed: null ,
+//         ),
+//
+//       ),
+//     );
+//   }
+// }
